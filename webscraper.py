b4c03bb2bc8b5ae2472830dcd62175d4188c158f
Caroline D <cdavis469@gmail.com>
2021-05-09 23:49:27 -0400

    Web Scraper - v1

--- webscraper.py
+++ webscraper.py
@@ -9,19 +9,43 @@ from bs4 import BeautifulSoup
 page = requests.get("https://www.espn.com/mma/fightcenter")
 soup = BeautifulSoup(page.content, "html.parser")
 
-soup_list = list(soup.children)
-html = soup_list[3]
-body = list(html.children)[3]
-p_tag = list(body.children)
-p = list(body.children)[1]
+# soup_list = list(soup.children)
+# html = soup_list[3]
+# body = list(html.children)[3]
+# p_tag = list(body.children)
+# p = list(body.children)[1]
 
+fight_stats = []
+for item in soup.find_all(class_="Accordion"):
+    fight_stats.append(item.get_text())
 
-data = soup.find(class_="AccordionPanel__body")
+fighter_names = []
+for item in soup.find_all(class_="truncate"):
+    fighter_names.append(item.get_text())
+
 
 # Extract:
 
-data_display = data.get_text()
-print(data_display)
+print(fighter_names)
+print(fight_stats)
+fight_stat_dict = {"Fighters: " : "",
+                   "Total Strikes: " : "",
+                   "Significant Strikes: " : "",
+                   "Head Strikes: " : "",
+                   "Body Strikes: " : "",
+                   "Leg Strikes: " : "",
+                   "Time in Control: " : "",
+                   "Takedowns: " : "",
+                   "Submissions: " : ""}
+
+for fighters in fighter_names:
+    for index in range(0, 2):
+        fight_stat_dict["Fighters: "] += fighter_names[index]
+    fighter_names = fighter_names[2:]
+
+print(fight_stat_dict)
+
+
 
 
 
